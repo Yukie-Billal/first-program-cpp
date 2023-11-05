bde24b162204c2fbbf4e69d79e33f35e155e50f9
Yukie M Billal <yukiembillal01@gmail.com>
2023-11-05 22:17:45 +0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int square(int count) {
+    return count * count;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << square(2) << std::endl;
+    std::cout << square(2) << std::endl;
+    std::cout << square(2) << std::endl;
+    return 0;
+}
